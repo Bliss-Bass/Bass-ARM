c9e4648b8c593edf536b4363f0a0d62d3d2339c4
electrikjesus <electrikjesus@gmail.com>
2024-04-12 17:47:17 -0400

    product: include device configs for gd_cm4

---
+++ build_cm4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+trap 'echo -e "\nbuild.sh interrupted"; exit 1' SIGINT
+
+echo Building the Android
+pushd aosptree
+. build/envsetup.sh
+lunch gd_cm4-userdebug
+make images -k || make images -j1
+make sdcard
+make otapackage
