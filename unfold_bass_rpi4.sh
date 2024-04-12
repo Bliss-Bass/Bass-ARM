29a23186c0d92066ee301da9793ba99f5fb6663b
electrikjesus <electrikjesus@gmail.com>
2024-04-12 18:38:03 -0400

    manifests: updates for bass unfolding

--- unfold_bass_rpi4.sh
+++ unfold_bass_rpi4.sh
@@ -32,7 +32,8 @@ top_dir=`readlink -f "$LOCAL_PATH/aosptree"`
 
 echo -e "${ltblue}Init repo tree using AOSP manifest ${reset}"
 pushd aosptree
-repo init -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs
+# repo init -u git@github.com:BlissBass-Private/bass-os.git -b bass-rpi-13.0 --git-lfs
+repo init -u https://android.googlesource.com/platform/manifest -b refs/tags/android-13.0.0_r35
 cd .repo/manifests
 mv default.xml aosp.xml
 cp ${LOCAL_PATH}/manifests/bass.xml bass.xml
